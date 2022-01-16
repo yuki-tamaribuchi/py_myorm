4b9b803c9c8109cb491c716c9944c41d67bb167a
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-17 00:20:31 +0900

    Update

--- src/main.py
+++ src/main.py
@@ -1,12 +1,25 @@
-from framework.operates.migrations import migrate_all_model
-migrate_all_model()
+#テーブル作成サンプル
+#from framework.operates.migrations import migrate_all_model
+#migrate_all_model()
 
 from models.users import Users
 from models.entries import Entries
 
+#インサートサンプル
+#Users().objects.create(username='yuki', handle='ゆうき').run()
 
-#users = Users().objects.create(username='yuki', handle='ゆうき')
-
-#users = Users().objects.get(username="yuki")
+#WHERE条件(1つ)指定セレクトサンプル
+#users = Users().objects.get(username="yuki").run()
 #print(users)
 
+#WHERE条件(2つ)指定セレクトサンプル
+#users = Users().objects.get(username="yuki", handle="ゆうき").run()
+#print(users)
+
+#WHERE条件3(つ)指定セレクトサンプル
+#users = Users().objects.get(username="sampleuser", handle="sample user", id=1).run()
+#print(users)
+
+#全セレクトサンプル
+#users = Users().objects.all().run()
+#print(users)
