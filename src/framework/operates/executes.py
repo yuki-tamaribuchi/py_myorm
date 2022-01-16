bc3fef106319c75951040cc08deb2b4aec9d2870
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-17 00:18:32 +0900

    Modify to execute and fetch

--- src/framework/operates/executes.py
+++ src/framework/operates/executes.py
@@ -5,37 +5,15 @@ from framework.exceptions.connection import DatabaseCreatedException
 from framework.connections.connection import connect
 
 
-def execute(sql, data=None):
-	try:
-		with connect() as cnx:
-			try:
-				with cnx.cursor() as cursor:
-					cursor.execute(sql, data)
-					cnx.commit()
-
-				#cursor = 
-				#cursor.execute(sql, data)
-				#cnx.commit()
-			except	mysql.connector.Error as err:
-				if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
-					print('already exists.')
-				else:
-					raise err
-	except DatabaseCreatedException:
-		print('Please try again')
-
-
-def execute_select(sql, data=None):
-	results = []
+def execute(sql):
 	try:
 		with connect() as cnx:
 			try:
 				with cnx.cursor() as cursor:
 					cursor.execute(sql)
-					
-					for result in cursor:
-						results.append(result)
-				return results
+					cnx.commit()
+				return True
+				
 			except	mysql.connector.Error as err:
 				if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
 					print('already exists.')
@@ -43,3 +21,17 @@ def execute_select(sql, data=None):
 					raise err
 	except DatabaseCreatedException:
 		print('Please try again')
+
+
+def fetch(sql):
+	results = []
+	try:
+		with connect() as cnx:
+			with cnx.cursor() as cursor:
+				cursor.execute(sql)
+				
+				for result in cursor:
+					results.append(result)
+				return results
+	except	mysql.connector.Error as err:
+		raise err
