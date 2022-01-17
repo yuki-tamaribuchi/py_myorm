8a13db71438ee366e4b398165d6f942b453843e6
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-18 00:07:32 +0900

    Create WhereClauseNotSpecifiedException and ResultNotOneException

--- src/framework/exceptions/select.py
+++ src/framework/exceptions/select.py
@@ -1,3 +1,11 @@
 class SelectFieldError(Exception):
 	def __init__(self, field_name):
 		print('Select field error: {}'.format(field_name))
+
+class WhereClauseNotSpecifiedException(Exception):
+	def __init__(self, model_name):
+		print('Where clause was not specified: {}'.format(model_name))
+
+class ResultNotOneException(Exception):
+	def __init__(self):
+		print("Results are not one. Please add more where clause or use filter function.")
